1792d5f63709e61097fef02d960e5fbb4f89d27f
Troy <tgruetzm@gmail.com>
2020-11-06 13:05:13 -0700

    restructure

--- PortfolioGenerator/PortfolioGenerator/Program.cs
+++ PortfolioGenerator/PortfolioGenerator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace PortfolioGenerator
 {
@@ -6,8 +8,76 @@ namespace PortfolioGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string baseDir = @"C:\Users\tgrue\OneDrive\Projects\Troy Gruetzmacher\www";
+            try
+            {
+                using (var sr = new StreamReader(baseDir + @"\index.template.html"))
+                {
+                    using (StreamWriter outputFile = new StreamWriter(baseDir + @"\index.html"))
+                    {
+                        string s = null;
 
+                        while ((s = sr.ReadLine()) != null)
+                        {
+                            outputFile.WriteLine(s);
+                            if(s == "<!-- Photo Grid -->")//start generation process
+                            {
+                                List<string> column1 = new List<string>();
+                                List<string> column2 = new List<string>();
+                                string[] fileEntries = Directory.GetFiles(baseDir + @"\Images\Portfolio");
+
+                                Dictionary<int, string> fileDict = new Dictionary<int, string>();
+                                foreach(string fullFile in fileEntries)
+                                {
+                                    string[] fileArray = fullFile.Split('\\');
+                                    string file = fileArray[fileArray.Length - 1];
+                                    string[] fileNameArray = file.Split('-');
+                                    int number = int.Parse(fileNameArray[fileNameArray.Length - 1].Replace(".jpg",""));
+                                    fileDict.Add(number, file);
+                                }
+
+
+                                for (int key = 1; key <= fileDict.Keys.Count; ++key)
+                                {
+                                    if ((key % 2) == 1)
+                                        column1.Add(fileDict[key]);
+                                    else
+                                        column2.Add(fileDict[key]);
+                                }
+                                //column 1
+                                outputFile.WriteLine("<div class=\"w3-half\">");
+                                foreach(string file in column1)
+                                {
+                                   
+                                    outputFile.WriteLine("<a target = \"_blank\" href = \"Images/Portfolio/"+file+"\" >");
+                                    outputFile.WriteLine("<img src = \"Images/Portfolio/" + file+ "\" loading = \"lazy\" style = \"width:100% \" >");
+                                    outputFile.WriteLine("</a>");
+             
+                                }
+                                outputFile.WriteLine("</div>");
+                                //column 2
+                                outputFile.WriteLine("<div class=\"w3-half\">");
+                                foreach (string file in column2)
+                                {
+
+                                    outputFile.WriteLine("<a target = \"_blank\" href = \"Images/Portfolio/" + file + "\" >");
+                                    outputFile.WriteLine("<img src = \"Images/Portfolio/" + file + "\" loading = \"lazy\" style = \"width:100% \" >");
+                                    outputFile.WriteLine("</a>");
+
+                                }
+                                outputFile.WriteLine("</div>");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
         }
+
     }
+
 }
